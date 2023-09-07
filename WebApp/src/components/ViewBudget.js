b36e18868abfeb51e4994a4e0a72891a8e7ae8c2
davids700 <dsolano7@csu.fullerton.edu>
2023-09-06 20:58:03 -0700

    Version 1
    Implemented version one.

---
+++ WebApp/src/components/ViewBudget.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ViewBudget = (props) => {
+  return (
+    <>
+      <span>Budget: ${props.budget}</span>
+      <button
+        type="button"
+        class="btn btn-primary"
+        onClick={props.handleEditClick}
+      >
+        Edit
+      </button>
+    </>
+  );
+};
+
+export default ViewBudget;
